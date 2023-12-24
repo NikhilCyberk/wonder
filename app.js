38e17431c91311c3f4e4cdd49b660ef70c94c7cf
NikhilCyberk <knikhil128@gmail.com>
2023-12-24 23:17:32 +0530

    passport in app.js

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const localStrategy = require("passport.local");
+const User = require("./models/user.js");
 
 let MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -48,6 +51,15 @@ app.get("/", (req, res) => {
 
 app.use(session(sessionoptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
