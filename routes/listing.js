4fa6f2d818c419b93f41c1df901993568f3f3e66
NikhilCyberk <knikhil128@gmail.com>
2023-12-25 17:49:44 +0530

    implementing MVC (model, view, controller) design pattern for listings

--- routes/listing.js
+++ routes/listing.js
@@ -3,65 +3,23 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingController = require("../controller/listings.js");
 
 //index routes
-router.get(
-  "/",
-  wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("./listings/index.ejs", { allListings });
-  })
-);
+router.get("/", wrapAsync(listingController.index));
 
 //new route
-router.get("/new", isLoggedIn, (req, res) => {
-  // console.log("req.user");
-  res.render("./listings/new.ejs");
-});
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 //show route
-router.get(
-  "/:id",
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id)
-      .populate({
-        path: "reviews",
-        populate: {
-          path: "author",
-        },
-      })
-      .populate("owner");
-    if (!listing) {
-      req.flash("error", "listing not found");
-      res.redirect("/listings");
-    }
-    // console.log(listing);
-    res.render("./listings/show.ejs", { listing });
-  })
-);
+router.get("/:id", wrapAsync(listingController.showListing));
 
 //create a new listing
 router.post(
   "/",
   isLoggedIn,
   validateListing,
-  wrapAsync(async (req, res, next) => {
-    // if (!req.body.listing) {
-    //   throw new ExpressError(400, "Send valid data for listing");
-    // }
-    // let result = listingSchema.validate(req.body);
-    // console.log(result);
-    // if (result.error) {
-
-    // }
-
-    const newListing = new Listing(req.body.listing);
-    newListing.owner = req.user._id;
-    await newListing.save();
-    req.flash("success", "Listing saved!");
-    res.redirect("/listings");
-  })
+  wrapAsync(listingController.createListing)
 );
 
 //edit routes
@@ -69,15 +27,7 @@ router.get(
   "/:id/edit",
   isLoggedIn,
   isOwner,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    if (!listing) {
-      req.flash("error", "listing not found");
-      res.redirect("/listings");
-    }
-    res.render("listings/edit.ejs", { listing });
-  })
+  wrapAsync(listingController.renderEditForm)
 );
 
 // Update
@@ -86,36 +36,14 @@ router.put(
   isLoggedIn,
   isOwner,
   validateListing,
-  wrapAsync(async (req, res) => {
-    if (!req.body.listing) {
-      throw new ExpressError(400, "Send valid data for listing");
-    }
-    let { id } = req.params;
-
-    // let listing = await Listing.findById(id);
-    // // console.log(listing.owner._id);
-    // if (!listing.owner._id.equals(res.locals.currUser._id)) {
-    //   req.flash("error", "you don't have permission to edit");
-    //   return res.redirect(`/listings/${id}`);
-    // }
-
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing }); //delistings
-    req.flash("success", "Listing updated");
-    res.redirect(`/listings/${id}`);
-  })
+  wrapAsync(listingController.updateListing)
 );
 // Delete Route
 router.delete(
   "/:id",
   isLoggedIn,
   isOwner,
-  wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    // console.log(deletedListing);
-    req.flash("success", "Listing deleted");
-    res.redirect("/listings");
-  })
+  wrapAsync(listingController.destroyListing)
 );
 
 module.exports = router;
