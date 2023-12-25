0241830d0932d9758cc3ced4c3dcfa50e81427fd
NikhilCyberk <knikhil128@gmail.com>
2023-12-25 15:25:10 +0530

    authorization for listing

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const Listing = require("./models/listing");
+const { listingSchema } = require("./schema.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { reviewSchema } = require("./schema.js");
+
 module.exports.isLoggedIn = (req, res, next) => {
   // this check that user is logged in or not
   // console.log(req.user);
@@ -15,4 +20,39 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     res.locals.redirectUrl = req.session.redirectUrl;
   }
   next();
+};
+//check if user is owner  or not
+module.exports.isOwner = async (req, res, next) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  // console.log(listing.owner._id);
+  if (!listing.owner._id.equals(res.locals.currUser._id)) {
+    req.flash("error", "Access denied");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
+};
+
+//for validating new lisitng for sending to the server
+module.exports.validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  //   console.log(result);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+//for validating new reviews for sending to the server
+module.exports.validateReviws = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  //   console.log(result);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
 };
