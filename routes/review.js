ff800bf277f18e20eeed4f5669aab12371d45255
NikhilCyberk <knikhil128@gmail.com>
2023-12-25 16:08:59 +0530

    authentication and authorization for reviews

--- routes/review.js
+++ routes/review.js
@@ -5,20 +5,26 @@ const ExpressError = require("../utils/ExpressError.js");
 
 const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
-const { validateReviws } = require("../middleware.js");
+const {
+  validateReviws,
+  isLoggedIn,
+  isReviewAuther,
+} = require("../middleware.js");
 
 // / Reviews
 // POST Route
 router.post(
   "/",
+  isLoggedIn,
   validateReviws,
   wrapAsync(async (req, res) => {
     // console.log(req.params.id);
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
 
     listing.reviews.push(newReview);
-
+    // console.log(newReview);
     await newReview.save();
     await listing.save();
 
@@ -32,6 +38,8 @@ router.post(
 //delete reviews route
 router.delete(
   "/:reviewId",
+  isLoggedIn,
+  isReviewAuther,
   wrapAsync(async (req, res) => {
     let { id, reviewId } = req.params;
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
